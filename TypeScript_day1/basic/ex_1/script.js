a4a118c4e4672bf13865e5a6b1674e449b32e41d
emo <e_karov@yahoo.com>
2023-03-02 11:10:23 +0100

    Add basic exercise 1

---
+++ TypeScript_day1/basic/ex_1/script.js
@@ -0,0 +1,7 @@
+"use strict";
+for (let i = 1; i <= 10; i++) {
+    for (let k = 1; k <= 10; k++) {
+        let output = `${k} X ${i} = ${k * i}`;
+        console.log(output);
+    }
+}
---
+++ TypeScript_day1/basic/ex_1/script.ts
@@ -0,0 +1,7 @@
+
+for (let i = 1; i <= 10; i++) {
+   for (let k = 1; k <= 10; k++) {
+    let output: String =  `${k} X ${i} = ${k*i}`;
+    console.log(output);
+   }
+}
